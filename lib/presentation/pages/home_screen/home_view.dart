ed4d9f0a442cd644c9c1cd955fe9fda4cf08205b
ishaque <ishaque@mobikasa.com>
2022-12-12 10:05:52 +0530

    initial commit on signup

--- lib/presentation/pages/home_screen/home_view.dart
+++ lib/presentation/pages/home_screen/home_view.dart
@@ -1,15 +1,71 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:task/app/utils/app_colors.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Center(child: Text("Home View")),
-        
-      ),
+          bottomNavigationBar: AnimatedBottomNavigationBar(
+              icons: const [Icons.home, Icons.category_outlined, Icons.person],
+              activeIndex: 0,
+              onTap: (index) => {}),
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                centerTitle: true,
+                floating: true,
+                title: const Text('M Y H O M E P A G E'),
+                expandedHeight: 300,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.pink,
+                        borderRadius: BorderRadius.circular(30)),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: 300,
+                      color: CustomColors.themeColor,
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: 300,
+                      color: CustomColors.themeColor,
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: 300,
+                      color: CustomColors.themeColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
